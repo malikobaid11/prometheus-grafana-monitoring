62d01d2a01a362e1dd464063d6fa4b93cea37444
Malik Obaid <47298615+malikobaid11@users.noreply.github.com>
2025-01-17 13:03:17 +0100

    Update install_grafana.sh

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get install -y adduser libfontconfig
+
+# Download and install Grafana
+wget https://dl.grafana.com/oss/release/grafana_5.0.4_amd64.deb
+sudo dpkg -i grafana_5.0.4_amd64.deb
+
+# Enable and start Grafana
+sudo systemctl enable --now grafana-server
 
