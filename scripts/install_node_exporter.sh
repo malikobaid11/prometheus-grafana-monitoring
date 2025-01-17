748259360b2da466048758d389bc9f32b41d6343
Malik Obaid <47298615+malikobaid11@users.noreply.github.com>
2025-01-17 13:03:02 +0100

    Update install_node_exporter.sh

--- scripts/install_node_exporter.sh
+++ scripts/install_node_exporter.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+# Download Node Exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_exporter-0.16.0.linux-amd64.tar.gz
+tar xvf node_exporter-0.16.0.linux-amd64.tar.gz
+
+# Install binary
+sudo cp node_exporter-0.16.0.linux-amd64/node_exporter /usr/local/bin
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+# Cleanup
+rm -rf node_exporter-0.16.0.linux-amd64*
 
